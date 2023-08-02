8e44105bd4618d4b4e694ce0749409de052e1b9c
TecHeReTiC <talex73@mail.ru>
2023-08-03 01:50:03 +0500

    Firestore has been connected and used for storing vans

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore/lite';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC6vvQSMKvroT5aeQ5s-VImfwytCuBLSFE",
+    authDomain: "vanlife-5cfcc.firebaseapp.com",
+    projectId: "vanlife-5cfcc",
+    storageBucket: "vanlife-5cfcc.appspot.com",
+    messagingSenderId: "285252701436",
+    appId: "1:285252701436:web:47f97cec6b4e46d4575b79"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
