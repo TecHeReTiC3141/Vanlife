d39e1cd0bb7f7c7f7fe99a7ec960e0e13bb640db
TecHeReTiC <talex73@mail.ru>
2023-08-05 15:21:16 +0500

    AuthProvider of current user has been implemented

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { auth } from '../../firebase';
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, } from 'firebase/auth';
+import {redirect} from "react-router-dom";
+
+const AuthContext = React.createContext();
+
+export function getAuth() {
+    return React.useContext(AuthContext);
+}
+
+function AuthProvider({ children }) {
+
+    const [currentUser, setCurrentUser] = React.useState(null);
+    const [loading, setLoading] = React.useState(true);
+
+    React.useEffect(() => {
+        return auth.onAuthStateChanged(user => {
+            setCurrentUser(user);
+            setLoading(false);
+        })
+    }, []);
+
+    async function signup(email, password) {
+        try {
+            await createUserWithEmailAndPassword(auth, email, password);
+            return redirect('/profile');
+        } catch (err) {
+            return err.message;
+        }
+    }
+
+    async function login(email, password) {
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            return redirect('/profile');
+        } catch (err) {
+            return err.message;
+        }
+    }
+
+    const value = {
+        currentUser,
+        signup,
+        login,
+    }
+
+    return (
+        <AuthContext.Provider value={value}>
+            { children }
+        </AuthContext.Provider>
+    );
+}
+
+export default AuthContext;
