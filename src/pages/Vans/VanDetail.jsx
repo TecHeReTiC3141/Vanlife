1296e9cb0ef690262036d25900c06cf2f211ac7b
TecHeReTiC <talex73@mail.ru>
2023-08-17 16:01:23 +0500

    Form for adding reviews of vans has been created

--- src/pages/Vans/VanDetail.jsx
+++ src/pages/Vans/VanDetail.jsx
@@ -1,11 +1,22 @@
 import React from "react"
-import { Link, useParams, useLocation, useLoaderData } from "react-router-dom"
-import { getVan } from "../../../api"
+import {Link, useParams, useLocation, useLoaderData, Form} from "react-router-dom"
+import {getVan} from "../../../api";
+import StarRatings from "react-star-ratings";
 
 // TODO: Add button which leads to Add review form
 
-export function loader({ params }) {
+export function loader({params}) {
     return getVan(params.id)
+}
+
+export const action = AuthContext => async ({ request, }) => {
+    const { currentUser } = AuthContext;
+
+    const formData = await request.formData();
+    const rating = +formData.get('rating'),
+    review = formData.get('review');
+    console.log(rating, review);
+
 }
 
 export default function VanDetail() {
@@ -14,6 +25,13 @@ export default function VanDetail() {
 
     const search = location.state?.search || "";
     const type = location.state?.type || "all";
+
+    const [rating, setRating] = React.useState(0);
+
+    function handleRatingChange(newRating) {
+        setRating(newRating);
+        document.getElementById('rating').value = newRating;
+    }
 
     return (
         <div className="van-detail-container">
@@ -24,14 +42,52 @@ export default function VanDetail() {
             >&larr; <span>Back to {type} vans</span></Link>
 
             <div className="van-detail">
-                <img src={van.imageUrl} />
+                <img src={van.imageUrl}/>
                 <i className={`van-type ${van.type} selected`}>
                     {van.type}
                 </i>
                 <h2>{van.name}</h2>
                 <p className="van-price"><span>${van.price}</span>/day</p>
                 <p>{van.description}</p>
-                <button className="link-button">Rent this van</button>
+                <div className="flex justify-between w-full gap-4">
+                    <button className="link-button  grow">Rent this van</button>
+                    <button className="link-button grow bg-blue-400">
+                        <Link to="add_comment">Add review of this van</Link>
+                    </button>
+                </div>
+
+                <h2 className="text-center my-4 underline">Add review of this van</h2>
+                <Form className="w-full flex justify-around" method="POST">
+                    <div className="">
+                        <h3 className="font-bold">How would you rate quality of this van?</h3>
+                        <StarRatings
+                            rating={rating}
+                            starRatedColor="orange"
+                            changeRating={handleRatingChange}
+                            numberOfStars={5}
+                            name='rating'
+                            className="block"
+                        />
+
+                        <button className="link-button mt-4">
+                            Add review of this van
+                        </button>
+                    </div>
+
+                    <input type="number" min="0" max="5" required
+                           id="rating" name="rating" className="hidden"/>
+
+                    <div className="">
+                        <h3 className="font-bold text-center mb-4">Write some words about this van</h3>
+
+                        <textarea name="review" id="review" cols="40" rows="10"
+                                  placeholder="Your review" className="rounded-md p-2" ></textarea>
+
+                    </div>
+
+
+                </Form>
+
             </div>
 
         </div>
